cd50c2c9f83a82e34e31fcac2a80f2442d6c5798
Carlos <csalgado307@gmail.com>
2019-11-08 08:58:32 -0800

    deploy now
    changes

--- js/script.js
+++ js/script.js
@@ -1,119 +1,86 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+var quotes = [
+  {quote:"She had magic in her eyes even the starts envied.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Bonita"},
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+  {quote:"& then I met you, & slowly but all at once my whole world began to change.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Princesa"},
+
+  {quote:"As I lay awake, the only thing that's on my mind, is you.",
+  source:"Tu Carlitos",
+  tag: "Te amo mi Reina"},
+
+  {quote:"Me volvi adicto a unos ojos verdes que brillan mas que una esmeralda.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Corazon"},
+
+  {quote:"Desde que te conoci descubri que puedo tener una sonrisa que dura 24 horas.. Gracias.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Cariño"},
+
+  {quote:"Descansa tu cabeza en mi pecho y escucharas a mi corazon decir que eres mas que suficiente.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Alma"},
+
+  {quote:"Solo quiero que sepas que no quiero estar con nadie mas que no seas tu.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Todo"},
+
+  {quote:"Por si lees esto: Amo cuando sonries.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Hermosa"},
+
+  {quote:"Eres mi felicidad dia a dia.",
+  source:"Tu Carlitos",
+  tag:"Te amo mi Universo"},
+
+  {quote:"Si tu me recuerdas, no me importa que el resto del mundo me olvide.",
+  source:"Tu Carlitos",
+  tag:"Mayo 24, 2019"},
+
+  {quote:"Tus hermosos ojos son una ventana a tu alma y lo que veo es una alma noble, amable, cariñosa, simplemente hermosa.",
+  source:"Tu Carlitos",
+  tag:"5-24-2019 para simepre"},
+
+  {quote:"Te encontre y no voy a dejarte ir, nadie se compara contigo.",
+  source:"Tu Carlitos",
+  tag:"Juntos para siempre mi Bonita"},
+
+  {quote:"Mientras te contemplo pienso en lo feliz que soy contigo.",
+  source:"Tu Carlitos",
+  tag:"Te amo Mi Bonita"}
+];
 
 
-/*** 
-  An array of objects that contains different quotes and properties of those quotes
-***/
+// function to choose a random quote from the array.
 
-const quotes = [
-{
-  quote : "I knew that if I failed I wouldn't regret that, but I knew the one thing I might regret is not trying.",
-  source : "Jeff Besos",
-  category: "Motivational",
-  year : 2015
-},
-
-{
-  quote: "Luck is a dividend of sweat. The more you sweat, the luckier you get.",
-  source:"Ray Croc",
-  category: "Motivational",
-  year:1962
-},
-
-{
-  quote : "Be yourself; everyone else is already taken.",
-  source :"Oscar Wilde",
-  citation : "The Wit & Wisdom of Oscar Wilde",
-  year : 1947
-  
-},
-
-{
-
-  quote : "Drunkenness is nothing but voluntary madness",
-  source : "Seneca",
-  citation : "Letters from a stoic",
-  category: "Humor",
-  year : "63 AD" 
-},
-
-{
-
-  quote : "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
-  source : "Martin Luther King Jr.",
-  citation : " A Testament of Hope: The Essential Writings and Speeches"
-  
+function getRandomQuote() {
+  let randomQuote = Math.floor(Math.random()*(quotes.length))
+  return quotes[randomQuote]
 }
-  
-
-]
-
-// Changes Background Color for every click
-console.log(quotes)
-const changeBgColor = () => {
-  let q = Math.floor(Math.random() * 256);
-  let w = Math.floor(Math.random() * 256);
-  let z = Math.floor(Math.random() * 256);
-  let randomBg = 'rgb(' + q + ',' + w + ',' + z + ')';
-  
-
-  document.body.style.background = randomBg;
-};
 
 
+// printQuote code to make it appear on the page
 
-//Generates a random number to randomize the index that is being selected
-const getRandomQuote = () => {
-let randomNumber = Math.floor(Math.random() * 5); 
+function printQuote(){
+  quoteObject = getRandomQuote();
+      printedText = "</p>" +"</p><p class=\"tag\"> " + quoteObject.tag + "</p>" + "<p class=\"quote\">" + quoteObject.quote + "</p><p class=\"source\"> " + quoteObject.source;
+      //text as it'll appear in box
+      document.getElementById('quote-box').innerHTML = printedText;
+      //make it appear on the page in the quote-box
 
-return quotes[randomNumber];
+}
 
-};
-
-
-
-
-
-
-// Prints out random quotes on the page by adding html strings into the 'quote-box div'
-const printQuote = () => {
- 
-  let randomQuote = getRandomQuote();
-  let htmlString = "";
-  
-  htmlString += `<p class="quote">${randomQuote.quote}`;
-  htmlString += `<p class="source">${randomQuote.source}`;
-  if (randomQuote.citation) {
-    htmlString += `<span class ='citation'>${randomQuote.citation}</span>`;
-  }
-  if (randomQuote.year) {
-    htmlString += `<span class ='year'>${randomQuote.year}</span>`;
-  }
-  if (randomQuote.category) {
-    htmlString += `<span class ='category'> , ${randomQuote.category}</span>`;
-  }
-  htmlString += '</p>';
-  document.getElementById('quote-box').innerHTML = htmlString;
-  changeBgColor();
-};
-  
-  
-
-
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-***/
-
+// event listener, responds to clicks on the loadQuote button from the html
+// when user clicks anywhere on the button, the "printQuote" function is called - activates the function to show random quote and change color
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
+//change background color on click
+document.getElementById("loadQuote").addEventListener("click", function rndColor(){
+    let red = Math.floor((Math.random() * 256));
+    let green = Math.floor((Math.random() * 256));
+    let blue = Math.floor((Math.random() * 256));
+    let color = 'rgb'+'('+red+','+green+','+blue+')';
+    document.body.style.backgroundColor = color;
+});
